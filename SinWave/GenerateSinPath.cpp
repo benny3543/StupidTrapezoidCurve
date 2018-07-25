f6c26a2f15343c12a3d085087a317c9d3b71ef6a
Bennett Lawson <bclawson3543@hotmail.com>
2018-07-25 09:15:51 -0400

    cout trig curves - no fileWrite

--- SinWave/GenerateSinPath.cpp
+++ SinWave/GenerateSinPath.cpp
@@ -1,14 +1,28 @@
+#define _USE_MATH_DEFINES
+
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
 //In deciseconds
-const static float MAX_ACCELERATION = 250
-const static float MAX_SPEED = 1100;
-const static float STEP_DURATION = 1.0F;
+//const static double MAX_ACCELERATION = 321.819;
+const static double MAX_SPEED = 971;
+const static double STEP_DURATION = 1.0;
+const static double TOTAL_DURATION = 150;
 
 int main() {
-	
+
+	int totalNumberOfSteps = TOTAL_DURATION / STEP_DURATION;
+
+	for (int i = 0; i <= totalNumberOfSteps; i++) {
+		double vel, pos;
+
+		vel = MAX_SPEED * sin((2 * M_PI * i) / TOTAL_DURATION);
+		pos = -MAX_SPEED * cos((2 * M_PI * i) / TOTAL_DURATION) + MAX_SPEED;
+
+		cout << "Position: " << pos << "   Velocity: " << vel << endl;
+	}
 
 	cin.get();
 	return 0;
